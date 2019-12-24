41888472a5fdab0af09934c9c62669d79d3d339f
HardCoreWare <bernardocodu@gmail.com>
2019-12-24 11:55:57 -0600

    preparando nuevo controlador y modelo

--- app/src/Modules/Concrete/Ceco.php
+++ app/src/Modules/Concrete/Ceco.php
@@ -8,27 +8,25 @@ class Ceco extends Connection{
 
   public function index(){
 
-    $sql =
-    "SELECT 
-    BELNR AS numeroDocumento, 
-    BLART AS tipoDocumento, 
-    PSWSL AS moneda, 
-    BUKRS AS sociedadSap, 
-    CAST(DMBTR AS FLOAT64) AS importe, 
-    KOSTL AS ceco, 
-    BKTXT AS descripcion, 
-    HKONT AS cuenta, 
-    CONCAT(SUBSTR(BUDAT,7,2),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,1,4)) AS fecha 
-    FROM `pit-analytics-2019.QLIK.Bsegaio` 
-    WHERE KOSTL IN (SELECT DISTINCT(ceco) FROM `pit-analytics-2019.QLIK.Ceco`) 
-    AND HKONT IN(SELECT DISTINCT(cuenta) FROM `pit-analytics-2019.QLIK.Cuentas`) 
-    ORDER BY CAST(BUDAT AS INT64) ;";
+    $sql ="SELECT
+    departamentos.idSucursal AS idDepartamento,
+    cecos.area AS area,
+    departamentos.ceco AS ceco,
+    cecos.sucursal AS departamento,
+  FROM
+    `pit-analytics-2019.QLIK.Ceco` AS departamentos
+  INNER JOIN
+    `pit-analytics-2019.QLIK.Cecos` AS cecos
+  ON
+    departamentos.ceco = cecos.numero
+  WHERE
+    1=1 ;";
     return $this->bigquery->query($sql);
 
   }
   public function fecha(){
 
-    $sql="SELECT DISTINCT(CONCAT(SUBSTR(BUDAT,7,2),'/',SUBSTR(BUDAT,5,2),'/',SUBSTR(BUDAT,1,4))) AS fecha FROM `pit-analytics-2019.QLIK.Bsegaio`;";
+    $sql="";
     return $this->bigquery->query($sql);
 
   }
