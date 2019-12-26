b214fb09514b4e6d212d62a30ba53d7360117ce2
HardCoreWare <bernardocodu@gmail.com>
2019-12-26 08:18:34 -0600

    agregando rutas cuenta

--- app/src/Modules/Concrete/Cuenta.php
+++ app/src/Modules/Concrete/Cuenta.php
@@ -41,6 +41,24 @@ class Cuenta extends Connection{
     return $this->bigquery->query($sql);
 
   }
+  public function concepto(){
+
+    $sql="SELECT DISTINCT(concepto) AS concepto 
+    FROM 
+    (SELECT 
+        Conceptos.id AS idConcepto, 
+        Conceptos.superConcepto AS superConcepto, 
+        Conceptos.concepto AS concepto, 
+        Cuentas.cuenta AS cuenta 
+      FROM
+        `pit-analytics-2019.QLIK.Conceptos` AS Conceptos
+      INNER JOIN
+        `pit-analytics-2019.QLIK.Cuentas` AS Cuentas
+      ON
+        Conceptos.id = Cuentas.idConcepto);";
+    return $this->bigquery->query($sql);
+
+  }
 
 }
 ?>
